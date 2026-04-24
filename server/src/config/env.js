eeb6e69e66355a9d6bcf06690e4ed35119a68cdc
Deovrat Singh <akashsingh08454@gmail.com>
2026-04-24 14:06:05 +0530

    Remove Dead Config

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -20,7 +20,6 @@ export const MONGO_URI = process.env.MONGO_URI;
 
 // JWT Configuration
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1d";
 
 // Email Configuration
 export const EMAIL_HOST = process.env.EMAIL_HOST || "smtp.gmail.com";
